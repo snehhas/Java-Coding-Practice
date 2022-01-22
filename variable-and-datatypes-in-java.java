27976726fd89b28bf4fedf402cbcfac95f767424
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2022-01-23 00:55:18 +0530

    Java

---
+++ variable-and-datatypes-in-java.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age=20;
+        String name= "Sneha";
+        float weight=60.5f;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
+    }
+}
